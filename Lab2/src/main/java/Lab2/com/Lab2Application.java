7077e6203cfd783692224c5059d2185e7970a0c0
Luchi <luyiddo@gmail.com>
2025-10-04 04:00:19 +0700

    noplab2

---
+++ Lab2/src/main/java/Lab2/com/Lab2Application.java
@@ -0,0 +1,13 @@
+package Lab2.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2Application.class, args);
+	}
+
+}
---
+++ Lab2/src/test/java/Lab2/com/Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Lab2.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
